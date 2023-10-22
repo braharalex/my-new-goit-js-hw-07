456ecfc3fdbafba745a398b3cec52f08cba64c36
braharalex <uljador@gmail.com>
2023-10-23 00:14:01 +0300

    primary changes

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(items) {
+ return items
+  .map(({ preview, original, description }) => {
+   return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+  })
+  .join("");
+}
+
+galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryEl.addEventListener("click", onClickOpenImage);
+
+function onClickOpenImage(evt) {
+ evt.preventDefault();
+
+ if (!evt.target.classList.contains("gallery__image")) {
+  return;
+ }
+
+ console.log("є");
+}
+
+const instance = basicLightbox.create(`
+    <h1>Dynamic Content</h1>
+    <p>You can set the content of the lightbox with JS.</p>
+`);
+
+console.log(instance);
+instance.show();
